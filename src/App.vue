e172e66b249de7092210114663e137bb302bae17
Jaime <jaimegarciagar94@gmail.com>
2022-08-20 16:31:05 +0200

    cambio titulo

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,8 @@ import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  <head>
-    <meta charset="utf-8" />
-    <link
-      href="https://api.fontshare.com/v2/css?f[]=general-sans@500&display=swap"
-      rel="stylesheet"
-    />
-  </head>
   <body class="bg-white dark:text-white dark:bg-gray-800 min-h-screen">
     <NavBar></NavBar>
     <Footer></Footer>
   </body>
 </template>
-
-<style>
-</style>
